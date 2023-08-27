4a76d05b0905bbe76bfa8709411994a88d4f2baa
Jake <jake.gonzalez@ttu.edu>
2023-08-27 00:05:20 -0600

    complete setup

--- lua/jake/plugins/nvim-cmp.lua
+++ lua/jake/plugins/nvim-cmp.lua
@@ -1,17 +1,25 @@
+-- import nvim-cmp plugin safely
 local cmp_status, cmp = pcall(require, "cmp")
 if not cmp_status then
   return
 end
 
-local lausnip_status, luasnip = pcall(require, "luasnip")
+-- import luasnip plugin safely
+local luasnip_status, luasnip = pcall(require, "luasnip")
 if not luasnip_status then
   return
 end
 
--- load friendly-snippets
+-- import lspkind plugin safely
+local lspkind_status, lspkind = pcall(require, "lspkind")
+if not lspkind_status then
+  return
+end
+
+-- load vs-code like snippets from plugins (e.g. friendly-snippets)
 require("luasnip/loaders/from_vscode").lazy_load()
 
-vim.opt.completopt = "menu,menuone,noselect"
+vim.opt.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
   snippet = {
@@ -20,17 +28,26 @@ cmp.setup({
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+    ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.abort(),
+    ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+    ["<C-e>"] = cmp.mapping.abort(), -- close completion window
     ["<CR>"] = cmp.mapping.confirm({ select = false }),
   }),
+  -- sources for autocompletion
   sources = cmp.config.sources({
+    { name = "nvim_lsp" }, -- lsp
     { name = "luasnip" }, -- snippets
-    { name = "buffer" }, -- text witin current buffer
-    { name = "path" }, -- file system path
+    { name = "buffer" }, -- text within current buffer
+    { name = "path" }, -- file system paths
   }),
+  -- configure lspkind for vs-code like icons
+  formatting = {
+    format = lspkind.cmp_format({
+      maxwidth = 50,
+      ellipsis_char = "...",
+    }),
+  },
 })
